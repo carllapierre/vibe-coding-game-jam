2ebec41aad6e00cf3a1e18f44ee63fa247287640
Carl Lapierre <carl.lapierre@osedea.com>
2025-03-21 20:26:01 -0400

    Update SpawnerRegistry to replace 'beef-raw' with 'burger-cheese-double' in the meat spawner configuration, enhancing item variety for spawning mechanics.

--- frontend/src/registries/SpawnerRegistry.js
+++ frontend/src/registries/SpawnerRegistry.js
@@ -6,7 +6,7 @@ export class SpawnerRegistry extends Registry {
         { id: 'boosts-1', cooldown: 10000, items: ['cup-coffee', 'carrot'],  },
         { id: 'bakery-1', cooldown: 10000, items: ['loaf', 'loaf-baguette', 'loaf-round', 'muffin', 'pancakes', 'mincemeat-pie', 'waffle'] },
         { id: 'bottles-1', cooldown: 5000, items: ['wine-white', 'wine-red', 'can'] },
-        { id: 'meat-1', cooldown: 5000, items: ['beef-raw', 'turkey', 'whole-ham'] },
+        { id: 'meat-1', cooldown: 5000, items: ['burger-cheese-double', 'turkey', 'whole-ham'] },
     ];
 
 }
